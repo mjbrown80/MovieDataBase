7a3c3f4ba5f1070d7cbec0679ea3f26f928e69cc
Matt Brown <mattbrown@Matts-MacBook-Pro.local>
2023-10-30 17:08:54 -0400

    update3

--- java/src/main/java/com/techelevator/controller/MovieController.java
+++ java/src/main/java/com/techelevator/controller/MovieController.java
@@ -1,4 +1,31 @@
 package com.techelevator.controller;
 
+import com.techelevator.dao.MovieDao;
+import com.techelevator.dao.UserDao;
+import com.techelevator.model.Movie;
+import com.techelevator.model.User;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@CrossOrigin
 public class MovieController {
+
+    private MovieDao movieDao;
+    private UserDao userDao;
+
+    public MovieController(MovieDao movieDao, UserDao userDao) {
+        this.movieDao = movieDao;
+        this.userDao = userDao;
+    }
+    @RequestMapping(path ="/movies", method = RequestMethod.GET)
+    public List<Movie> movies(Principal principal){
+        User user = userDao.getUserByUsername(principal.getName());
+        return movieDao.getMovies();
+    }
 }
