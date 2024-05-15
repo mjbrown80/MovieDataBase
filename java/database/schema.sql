b90319a46c16f208cbb4eef94c3561f973e92e45
Matt Brown <matthewjbrown365@gmail.com>
2024-05-15 13:01:49 -0400

    added collection to schema

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -2,6 +2,7 @@ BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS user_movie_collections;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -23,6 +24,16 @@ CREATE TABLE movies (
     imdb_id varchar(25),
     CONSTRAINT PK_movie PRIMARY KEY (movie_id)
     CONSTRAINT unique_movie_title_release_date UNIQUE (movie_title, movie_release_date)
+);
+
+CREATE TABLE user_movie_collections (
+    collection_id SERIAL,
+    user_id INT NOT NULL,
+    movie_id INT NOT NULL,
+    CONSTRAINT PK_user_movie_collections PRIMARY KEY (collection_id),
+    CONSTRAINT FK_user_movie_collections_user FOREIGN KEY (user_id) REFERENCES users(user_id),
+    CONSTRAINT FK_user_movie_collections_movie FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
+    CONSTRAINT unique_user_movie UNIQUE (user_id, movie_id)
 );
 
 COMMIT TRANSACTION;
